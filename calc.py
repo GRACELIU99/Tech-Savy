4df40d97a08f2fe0a7af353451bdb4544de47a42
Grace <kliu3@babson.edu>
2019-03-15 16:35:10 -0400

    finished class on Friday

---
+++ calc.py
@@ -0,0 +1,13 @@
+print ((3+32*9))
+
+
+print(2919-919)
+print(2000 - 2)
+
+print (3*8)
+print(-1-1)
+print ('1+2+3=', 1+2+3)
+name = 'Grace'
+print('Hello,', name)
+
+print('Pi equals {:.2f}'.format(3.141569))
