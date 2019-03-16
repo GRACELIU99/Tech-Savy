1306938e2f56124ff7086aab67e39ff07c390484
Grace <kliu3@babson.edu>
2019-03-16 13:36:45 -0400

    Day 2

---
+++ calc_day2.py
@@ -0,0 +1,72 @@
+NUMBER_OF_SECONDS_IN_ONE_MINUTE = 60
+seconds =42*60+42
+print("There are {} seconds ...". format(seconds))
+
+
+NUMBER_OF_SECONDS_IN_ONE_MINUTE = 60
+seconds = 42 * NUMBER_OF_SECONDS_IN_ONE_MINUTE+42
+print('There\'s {} seconds ...'. format (seconds))
+
+
+NUMBER_OF_KILOMETERS_IN_A_MILE = 1.61
+MILES = 10/NUMBER_OF_KILOMETERS_IN_A_MILE
+print ("There are {} miles in 10 kilometers...". format (MILES))
+
+
+NUMBER_OF_KILOMETERS_IN_A_MILE = 1.61
+MILES = 10/NUMBER_OF_KILOMETERS_IN_A_MILE
+print ("There are {:.2f} miles in 10 kilometers...". format (MILES))
+
+
+NUMBER_OF_KILOMETERS_IN_A_MILE = 1.61
+MILES = 10/NUMBER_OF_KILOMETERS_IN_A_MILE
+print ("There are {:.3f} miles in 10 kilometers...". format (MILES))
+
+
+#compute te percentage of te hour that has elapsed.
+minute = 45
+percentage = (minute*100)/60
+print('45 minutes is equal to {}% of one hour...'. format (percentage))
+
+#what is the volume of a sphere with radius 5
+import math
+r=5
+volume = (4/3)*math.pi*math.pow(r,3)
+print('The volume of the sphere {} is {:.3f}.'. format(r, volume))
+
+
+##
+print("My name is {:.1}.". format('Grace'))
+
+
+##
+import math
+r=input('Please enter a number')
+print(type(r))
+
+r=int(r)
+print('After conversion,', type(r))
+
+volume = (4/3)*math.pi*math.pow(r,3)
+print ('the volume of a sphere of {} is {:.3}. ' . format(r, volume))
+
+
+#1.3
+hour = 13
+min = 2
+print('Current time is {:2}:{:02}.'. format(hour , min))
+
+
+##
+print(1+2)
+
+
+##
+import datetime
+print(datetime.datetime.now())
+
+
+now = datetime. datetime.now()
+print(now)
+print(now.hour, now.minute, now.second)
+print (now. year)
---
+++ calc_homepractice.py
@@ -0,0 +1,27 @@
+print (46+37+38)
+print (2**6)
+print (15/3)
+print ('hello, Grace')
+print('hello World')
+print('Hey Jude', 'don\'t make it bad')
+print('The total number of overall medals in Rio 2016 is', 46+37+38)
+print('46+37+38 =', 46+37+38)
+name = input("What is your name?")
+name
+print(name)
+name = input("What is your name?")
+print('hello,', name)
+n=100
+print(n)
+
+
+a = 123 # a is an integer
+print(a)
+a = 'ABC' # a becomes a string
+print(a)
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+seconds = 42 *60 +42
+print('There are {} seconds...'.format(seconds))
